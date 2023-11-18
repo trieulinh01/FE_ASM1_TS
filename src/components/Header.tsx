e3f49dda015db9a00a6ec6d4bf10b8e6f2ce5b17
linh <linhttkph30456@fpt.edu.vn>
2023-11-18 21:41:06 +0700

    update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ export default function Header(){
 
   <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
     <div className="flex flex-wrap items-center justify-between max-w-screen-xl mx-auto">
-      <a href=""><img className="w-24 h-24" src="./../../public/logo.png" alt="" /></a>
+      <a href=""><img className="w-24 h-24" src="/logo.png" alt="" /></a>
       <div className="flex items-center lg:order-2">
         <Link to={'/login'} className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Sign in</Link>
         <Link to={'/signup'} className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Sign up</Link>
